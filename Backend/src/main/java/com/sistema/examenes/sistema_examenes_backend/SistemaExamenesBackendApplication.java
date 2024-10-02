aee4295a46d0d481d94355f89e684c3175565191
Randy <randy26smc@gmail.com>
2024-10-02 15:19:58 -0600

    Configuración inicial del backend

---
+++ Backend/src/main/java/com/sistema/examenes/sistema_examenes_backend/SistemaExamenesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.examenes.sistema_examenes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamenesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/sistema/examenes/sistema_examenes_backend/SistemaExamenesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.examenes.sistema_examenes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaExamenesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
