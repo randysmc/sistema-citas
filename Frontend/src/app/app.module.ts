874010aad70dade3cc04757f13a118b5ce676413
Randy <randy26smc@gmail.com>
2024-10-05 15:15:40 -0600

    componente para signup

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //importaciones de angular material
-import {MatButtonModule} from '@angular/material/button'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'; 
+
+
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit{
+
+  public user = {
+    username : '',
+    password : '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    dpi: '',
+    cui: '',
+
+  }
+
+  constructor(private userService:UserService){ }
+
+  ngOnInit(): void {
+    
+  }
+
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username == '' || this.user.username == null){
+      alert('El nombre del usuario es requerido');
+      return; 
+    }
+
+    this.userService.registrarUsuario(this.user).subscribe(
+      (data) =>  {
+        console.log(data);
+        alert('Usuario guardado con exito')
+      },(error) => {
+        console.log(error);
+        alert('ha ocurrido un error en el sistema')
+      }
+    )
+  }
+
+}
