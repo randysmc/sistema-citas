dbf1e2805bcead1f0f49ede983979b1a16bee465
Randy <randy26smc@gmail.com>
2024-10-26 09:56:26 -0600

    integracion con jacoco y jenkins

--- Backend/src/test/java/com/sistema/examenes/sistema_examenes_backend/SistemaExamenesBackendApplicationTests.java
+++ Backend/src/test/java/com/sistema/examenes/sistema_examenes_backend/SistemaExamenesBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.sistema.examenes.sistema_examenes_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SistemaExamenesBackendApplicationTests {
 
 	@Test
